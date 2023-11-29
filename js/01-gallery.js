5116070e1ee4fa306d479417c22b0bc5d093a14b
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2023-11-29 23:50:15 +0200

    do_task1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const galleryItemIndex = galleryItems.findIndex(
+    (item) => item.preview === target.getAttribute("src")
+  );
+
+  if (galleryItemIndex !== -1) {
+    openLightbox(galleryItemIndex);
+  }
+}
+
+function openLightbox(index) {
+  const largeImage = getLargeImage(index);
+
+  const lightbox = basicLightbox.create(`<img src="${largeImage}" alt="">`, {
+    onClose: () => {
+      document.removeEventListener("keydown", onKeyPress);
+    },
+  });
+
+  lightbox.show();
+
+  document.addEventListener("keydown", onKeyPress);
+
+  function onKeyPress(event) {
+    if (event.code === "Escape") {
+      lightbox.close();
+    }
+  }
+}
+
+function getLargeImage(index) {
+  return galleryItems[index].original;
+}
 
 console.log(galleryItems);
